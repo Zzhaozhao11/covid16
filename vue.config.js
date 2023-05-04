41999ab83c2d890dc2d8a2d554f4aadb1d98fd51
zhaozhao <164568105@qq.com>
2023-05-04 22:33:02 +0800

    my

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   lintOnSave:false,  // 是否开启eslint保存检测
   publicPath:'./', //根路径防止打包后出现空白
   devServer: {
-    host: "127.0.255.255", // 启动服务器域名
+    host: "127.0.0.1", // 启动服务器域名
     port: "1234", // 启动服务器端口号
     open: true, // 是否自动打开浏览器
   },
